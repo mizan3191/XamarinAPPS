f410ec5dbd2da1f9092012ce7ce0e5d6ab478a04
mizanur rahaman <mizanurrahaman020195@gmail.com>
2022-08-11 00:05:29 +0600

    save

---
+++ src/ProfileDemoApp/ProfileDemoApp/ProfileDemoApp/LoginUI.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProfileDemoApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginUI : ContentPage
+    {
+        public LoginUI()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if(usernameText.Text == "admin" && passwordText.Text == "admin123")
+            {
+                Navigation.PushAsync(new ProfileUI());
+            }
+            else
+            {
+                DisplayAlert("Warning", "Invalid UserName OR Password", "Ok");
+            }
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Register());
+        }
+    }
+}
